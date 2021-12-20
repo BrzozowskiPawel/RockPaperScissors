a7a2f9907da4f3a8165487598f34be037e6e6853
Paweł Brzozowski <mac@192.168.1.6>
2021-12-20 14:56:05 +0100

    added firebase functions

--- RockPaperScissors/RockPaperScissors/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+        
+        // Generating collection <rand id> add adding <users> documnet
+        db.collection("TEST").document("users").setData(["Host":"me2"], merge: true)
+        
+        // Listening for changes in "users"
+        db.collection("TEST").document("users").addSnapshotListener { docSnapshot, error in
+            if error == nil && docSnapshot != nil && docSnapshot!.data() != nil {
+                // No errors
+                print(docSnapshot!.data())
+                print(docSnapshot!.data()!.count)
+            }
+        }
     }
+    
 
 
 }
