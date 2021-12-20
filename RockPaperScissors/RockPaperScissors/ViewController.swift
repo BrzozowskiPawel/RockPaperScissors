e72dbc14f0a7360cf6897f60cf02434200baba50
Paweł Brzozowski <mac@192.168.1.6>
2021-12-20 12:32:25 +0100

    created project

---
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Paweł Brzozowski on 20/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
