37ae1a3b51f93e53340d1103651bedf6cf225d4b
Paweł Brzozowski <mac@192.168.1.6>
2021-12-20 19:19:56 +0100

    Setup logic path for each screen

---
+++ RockPaperScissors/RockPaperScissors/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RockPaperScissors
+//
+//  Created by Paweł Brzozowski on 20/12/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static var RESULT_SEGUE = "GoToResultsVC"
+}
