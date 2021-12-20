687a5b920f5f0db59b41b706ae0ee2decad9c89b
Paweł Brzozowski <mac@192.168.1.6>
2021-12-20 19:31:09 +0100

    UI finished on first screen

--- RockPaperScissors/RockPaperScissors/Controller/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/Controller/ViewController.swift
@@ -9,27 +9,40 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hostButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        let db = Firestore.firestore()
+        // Configuration of UI elements:
         
-        // Generating collection <rand id> add adding <users> documnet
-        db.collection("TEST").document("users").setData(["Host":"me2"], merge: true)
+        // Properly formated main label text:
+        nameLabel.text = "ROCK \n / PAPAER / \n SCISSORS"
         
-        // Listening for changes in "users"
-        db.collection("TEST").document("users").addSnapshotListener { docSnapshot, error in
-            if error == nil && docSnapshot != nil && docSnapshot!.data() != nil {
-                // No errors
-                print(docSnapshot!.data())
-                print(docSnapshot!.data()!.count)
-            }
-        }
+        // Set radius for both buttons:
+        hostButton.layer.cornerRadius = 10
+        joinButton.layer.cornerRadius = 10
     }
-    
 
 
 }
 
+// Firebase commands:
+//extension ViewController {
+//    let db = Firestore.firestore()
+//
+//    // Generating collection <rand id> add adding <users> documnet
+//    db.collection("TEST").document("users").setData(["Host":"me2"], merge: true)
+//
+//    // Listening for changes in "users"
+//    db.collection("TEST").document("users").addSnapshotListener { docSnapshot, error in
+//        if error == nil && docSnapshot != nil && docSnapshot!.data() != nil {
+//            // No errors
+//            print(docSnapshot!.data())
+//            print(docSnapshot!.data()!.count)
+//        }
+//    }
+//}
